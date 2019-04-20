8f2dc67464090d10518f88a9d7c00647fbe2bae2
rajareddy2507 <rajareddy2507@gmail.com>
2019-04-20 00:22:50 -0400

    Update HelloWorld.java

--- Selenium_Learning/src/main/java/HelloWorld.java
+++ Selenium_Learning/src/main/java/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World 2");
-		System.out.println("Checking for new branch");
+		System.out.println("Creating new branch");
 		System.out.println("Added new defect_fix branch");
 	}
 
