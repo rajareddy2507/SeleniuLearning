12521ca0ef34579a0ee3985f55bd6b549ee48a00
Raja Reddy Bolle <rajareddy2507@gmail.com>
2019-04-22 16:27:51 -0400

    added few sysout, modified few and deleted few

--- Selenium_Learning/src/main/java/HelloWorld.java
+++ Selenium_Learning/src/main/java/HelloWorld.java
@@ -2,14 +2,13 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("Hello World 1");
-		System.out.println("Checking for new branch");
-=======
-		System.out.println("Hello World 2");
+
+		System.out.println("Hello");
 		System.out.println("Creating new branch");
->>>>>>> 8f2dc67464090d10518f88a9d7c00647fbe2bae2
 		System.out.println("Added new defect_fix branch");
+		
+		System.out.println("######## Added new line#######");
+		
 	}
 
 }
