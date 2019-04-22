5c6946bb3c7616ae11e04320458fc4ebc45988bc
Raja Reddy Bolle <rajareddy2507@gmail.com>
2019-04-22 16:42:35 -0400

    Testing

--- Selenium_Learning/src/main/java/HelloWorld.java
+++ Selenium_Learning/src/main/java/HelloWorld.java
@@ -3,11 +3,9 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello");
-		System.out.println("Creating new branch");
-		System.out.println("Added new defect_fix branch");
-		
-		System.out.println("######## Added new line#######");
+		System.out.println("Hello ### Updated");
+		System.out.println("Creating new branch");		
+		System.out.println("********** Added new line*****");
 		
 	}
 
