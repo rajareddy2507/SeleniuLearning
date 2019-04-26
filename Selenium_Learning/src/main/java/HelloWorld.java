8b58f6ca1f62df520aa5cb2e5e0062e754d872f1
Raja Reddy Bolle <rajareddy2507@gmail.com>
2019-04-25 22:47:39 -0400

    Added new branch

--- Selenium_Learning/src/main/java/HelloWorld.java
+++ Selenium_Learning/src/main/java/HelloWorld.java
@@ -2,16 +2,16 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		Season season;
-		
-
+		Season season3;
 		System.out.println("Hello ### Updated");
 		System.out.println("Creating new branch");
 		System.out.println("********** Added new line*****");
-	season = Season.SPRING;
-	System.out.println(season);
-	
-	System.out.println(season.ordinal());
+	season3 = Season.SPRING;
+	Season season1 = Season.WINTER;
+	System.out.println(season3);
+	System.out.println(season1);
+	System.out.println(season3.ordinal());
+	System.out.println("Added new sysout");
 	
 	}
 
